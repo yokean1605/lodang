655a1f8dd9973282edefff8b62f7df54cb2a3504
Yoke Andreian Yudistiro <yoke.mahardika@gmail.com>
2020-09-22 08:53:05 +0700

    slider pilihan kategori

--- js/lodang.js
+++ js/lodang.js
@@ -1,41 +1,28 @@
 (function($) {
   "use strict"; // Start of use strict
-
-  // Floating label headings for the contact form
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
+  $('.scrollable').slick({
+    dots: false,
+    swipe: true,
+    arrows: true,
+    infinite: false,
+    variableWidth: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
   });
 
-  // Show the navbar when the page is scrolled up
-  var MQL = 992;
+  $('.scrollable').on('afterChange', function(event, slick, currentSlide) {
 
-  //primary navigation slide-in effect
-  if ($(window).width() > MQL) {
-    var headerHeight = $('#mainNav').height();
-    $(window).on('scroll', {
-        previousTop: 0
-      },
-      function() {
-        var currentTop = $(window).scrollTop();
-        //check if user is scrolling up
-        if (currentTop < this.previousTop) {
-          //if scrolling up...
-          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
-            $('#mainNav').addClass('is-visible');
-          } else {
-            $('#mainNav').removeClass('is-visible is-fixed');
-          }
-        } else if (currentTop > this.previousTop) {
-          //if scrolling down...
-          $('#mainNav').removeClass('is-visible');
-          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
-        }
-        this.previousTop = currentTop;
-      });
-  }
+    if (currentSlide === 2) {
+      $('.slick-next').addClass('hidden');
+    } else {
+      $('.slick-next').removeClass('hidden');
+    }
+
+    if (currentSlide === 0) {
+      $('.slick-prev').addClass('hidden');
+    } else {
+      $('.slick-prev').removeClass('hidden');
+    }
+  })
 
 })(jQuery); // End of use strict
