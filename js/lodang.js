092683cfa43e896e51e487b40c77f5fa44c6e86e
Yoke Andreian Yudistiro <yoke.mahardika@gmail.com>
2020-09-22 17:10:21 +0700

    add image kategori

--- js/lodang.js
+++ js/lodang.js
@@ -25,4 +25,23 @@
     }
   })
 
+  $('.filters ul li').on('click', function () {
+    $('.filters ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+        filter: data
+    })
+});
+
+var $grid = $(".grid").isotope({
+    itemSelector: ".all",
+    percentPosition: true,
+    masonry: {
+        columnWidth: ".all"
+    }
+})
+
+
 })(jQuery); // End of use strict
